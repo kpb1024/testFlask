ff6fc6db7146af60dccb2f3b2d66996cfd971abc
root <kangpeibang@qq.com>
2019-05-09 17:25:28 +0800

    添加Flask-JSGlue，是在js中可以使用url_for

--- ssms/__init__.py
+++ ssms/__init__.py
@@ -1,32 +1,20 @@
 import os
 
 from flask import Flask
-<<<<<<< HEAD
 from flask_sqlalchemy import SQLAlchemy
-=======
 from flask_jsglue import JSGlue
->>>>>>> 266f41eeae4d225dbf423f5cf25d955d8177b373
 
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
-<<<<<<< HEAD
-=======
     jsglue = JSGlue()
     jsglue.init_app(app)  # 让js文件中可以使用url_for方法
->>>>>>> 266f41eeae4d225dbf423f5cf25d955d8177b373
     app.config.from_mapping(
         # a default secret that should be overridden by instance config
         SECRET_KEY='dev',
         # store the database in the instance folder
         DATABASE=os.path.join(app.instance_path, 'ssms.sqlite'),
-<<<<<<< HEAD
-	SQLALCHEMY_DATABASE_URI='sqlite:////ssms.db',
     )
-    orm = SQLAlchemy(app)
-=======
-    )
->>>>>>> 266f41eeae4d225dbf423f5cf25d955d8177b373
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
